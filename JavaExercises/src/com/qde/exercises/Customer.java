86f051c39fbd4a69013b1063d1c16ee278e94b27
Decayeux <qdecayeux@natsystem.fr>
2018-11-26 19:39:17 +0100

    add customer getters setters

--- JavaExercises/src/com/qde/exercises/Customer.java
+++ JavaExercises/src/com/qde/exercises/Customer.java
@@ -3,5 +3,21 @@ package com.qde.exercises;
 public class Customer {
 	private String name;
 	private String address;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
+	
+	
+	
+	
 }
