bb2ea055ce2c41814b1f2b8f1c74ad4b050f37b0
Decayeux <qdecayeux@natsystem.fr>
2018-11-26 20:28:41 +0100

    adding quadrilateral superclass

--- JavaExercises/src/com/qde/exercises/geometry/Quadrilateral.java
+++ JavaExercises/src/com/qde/exercises/geometry/Quadrilateral.java
@@ -1,5 +1,27 @@
 package com.qde.exercises.geometry;
 
 public class Quadrilateral {
-
+	private double height;
+	private double width;
+	public double getHeight() {
+		return height;
+	}
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	public double getWidth() {
+		return width;
+	}
+	public void setWidth(double width) {
+		this.width = width;
+	}
+	
+	public double getArea() {
+		return this.getHeight() * this.getWidth();
+	}
+	
+	public double getPerimeter() {
+		return (this.getHeight() + this.getWidth()) * 2;
+	}
+	
 }
