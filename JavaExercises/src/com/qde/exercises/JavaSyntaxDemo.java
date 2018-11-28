45494c1915efad8d9f1fc9e7c3f48147a68ca6b7
Decayeux <qdecayeux@natsystem.fr>
2018-11-28 20:04:03 +0100

    login + menu nav added

--- JavaExercises/src/com/qde/exercises/JavaSyntaxDemo.java
+++ JavaExercises/src/com/qde/exercises/JavaSyntaxDemo.java
@@ -23,41 +23,43 @@ public class JavaSyntaxDemo {
 		return this.version;
 	}
 	
-	public static void main(String[] args){
-//		Customer customer1 = new Customer();
-//		customer1.setName("Smith");
-//		customer1.setAddress("3 baker street");
+//	public static void main(String[] args){
+////		Customer customer1 = new Customer();
+////		customer1.setName("Smith");
+////		customer1.setAddress("3 baker street");
+////
+////		Customer customer2 = new Customer("Higgins", "4 baker street");
+////		
+////		SavingsAccount account = new SavingsAccount();
+////		account.setBalance(500);
+////		account.setInterestRate(5);
+////		Double balance = new Double(account.getBalance());
+////		switch (balance.intValue()) {
+////		case 200 :
+////			System.out.println("You have 200 bucks");
+////		case 500 :
+////			System.out.println("You have 500 bucks");
+////		}
+////		
+////		
+////		System.out.println("Balance after interest computation : " + account.computeInterest());
+////		
+////		System.out.println("Balance before withdrawal : " + account.getBalance());
+////		account.withdraw(50);
+////		System.out.println("Balance after withdrawal : " + account.getBalance());
 //
-//		Customer customer2 = new Customer("Higgins", "4 baker street");
-//		
-//		SavingsAccount account = new SavingsAccount();
-//		account.setBalance(500);
-//		account.setInterestRate(5);
-//		Double balance = new Double(account.getBalance());
-//		switch (balance.intValue()) {
-//		case 200 :
-//			System.out.println("You have 200 bucks");
-//		case 500 :
-//			System.out.println("You have 500 bucks");
-//		}
-//		
-//		
-//		System.out.println("Balance after interest computation : " + account.computeInterest());
-//		
-//		System.out.println("Balance before withdrawal : " + account.getBalance());
-//		account.withdraw(50);
-//		System.out.println("Balance after withdrawal : " + account.getBalance());
-
-		Circle bigCircle = new Circle();
-		bigCircle.setRadius(60); // the radius is set thanks to the setter
-		Circle smallCircle = new Circle(20); // the radius is set thanks to the constructor
-		System.out.println("The small circle radius is : " + smallCircle.getRadius());
-		System.out.println("The big circle radius is : " + bigCircle.getRadius());
-		System.out.println("Small circle area : " + smallCircle.getArea());
-		System.out.println("Small circle perimeter : " + smallCircle.getPerimeter());
-		System.out.println("Big circle area : " + bigCircle.getArea());
-		System.out.println("Big circle perimeter : " + bigCircle.getPerimeter());
-    }
+////		Circle bigCircle = new Circle();
+////		bigCircle.setRadius(60); // the radius is set thanks to the setter
+////		Circle smallCircle = new Circle(20); // the radius is set thanks to the constructor
+////		System.out.println("The small circle radius is : " + smallCircle.getRadius());
+////		System.out.println("The big circle radius is : " + bigCircle.getRadius());
+////		System.out.println("Small circle area : " + smallCircle.getArea());
+////		System.out.println("Small circle perimeter : " + smallCircle.getPerimeter());
+////		System.out.println("Big circle area : " + bigCircle.getArea());
+////		System.out.println("Big circle perimeter : " + bigCircle.getPerimeter());
+//
+//	
+//	}
 	
 
 }
---
+++ JavaExercises/src/com/qde/exercises/login/Login.java
@@ -0,0 +1,50 @@
+package com.qde.exercises.login;
+
+import java.util.Scanner;
+
+public class Login {
+
+	
+	private static final String CORRECT_LOGIN = "admin";
+	private static final String CORRECT_PASSWORD = "password";
+
+	public static void main(String[] args) {
+		login();
+
+	}
+
+	private static void login() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter your login : ");
+		String userLoginInput = scanner.nextLine();
+		System.out.println("Please enter your password : ");
+		String userPasswordInput = scanner.nextLine();
+		if (CORRECT_LOGIN.equals(userLoginInput) 
+			&& CORRECT_PASSWORD.equals(userPasswordInput)) {
+			System.out.println("Congratulations, you are authenticated !");
+			
+			menuNavigation(scanner);
+			
+		} else {
+			System.out.println("Authentication failed.");
+		}
+	}
+
+	private static void menuNavigation(Scanner scanner) {
+		System.out.println("Please choose one of the 3 following options : Create, Modify, Delete");
+		String userMenuInput = scanner.nextLine();
+		switch(userMenuInput) {
+		case "Create" : 
+			System.out.println("You've entered the Create menu");
+			break;
+		case "Modify" :
+			System.out.println("You've entered the Modify menu");
+			break;
+		case "Delete" :
+			System.out.println("You've entered the Delete menu");
+			break;
+		default :
+			System.out.println("Incorrect input");
+		}
+	}
+}
