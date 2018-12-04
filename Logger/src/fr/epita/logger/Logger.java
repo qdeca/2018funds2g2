caea5b6b96cca481f177d0830db48d0c60d5e807
tbrou <thomas.broussard@gmail.com>
2018-12-04 20:24:41 +0100

    * integrated logger feature

---
+++ Logger/src/fr/epita/logger/Logger.java
@@ -0,0 +1,45 @@
+package fr.epita.logger;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Logger {
+
+	private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyyMMdd_HH:mm:ss.SSS");
+	private static boolean isInitialized = false;
+
+	private static PrintWriter pw;
+
+	public static void logMessage(String message) {
+		Date date = new Date();
+		if (!isInitialized) {
+			pw = initialize();
+		}
+		if (pw != null) {
+			pw.println(SIMPLE_DATE_FORMAT.format(date) + "-" + message);
+		}
+		pw.flush();
+	}
+
+	private static PrintWriter initialize(){
+		try {
+			File file = new File("application.log");
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			PrintWriter writer = new PrintWriter(new FileWriter(file, true));
+			isInitialized = true;
+			writer.println("initialized!");
+			return writer;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		isInitialized = false;
+		return null;
+
+	}
+
+}
---
+++ Logger/test/fr/epita/logger/tests/TestLogger.java
@@ -0,0 +1,12 @@
+package fr.epita.logger.tests;
+
+import fr.epita.logger.Logger;
+
+public class TestLogger {
+
+	
+	public static void main(String[] args) {
+		Logger.logMessage("hello!");
+		Logger.logMessage("this is to test the logger feature");
+	}
+}
