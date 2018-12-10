b99180cd79c034a885df487a98badd4259c123c4
tbrou <thomas.broussard@gmail.com>
2018-12-10 20:28:54 +0100

    * File Based DAO integration

--- DAOExercises/src/fr/epita/services/dao/CustomerFileDAO.java
+++ DAOExercises/src/fr/epita/services/dao/CustomerFileDAO.java
@@ -1,7 +1,62 @@
 package fr.epita.services.dao;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import fr.epita.datamodel.Customer;
+
 public class CustomerFileDAO {
 	
+	private PrintWriter writer;
+	private Scanner scanner;
+	
+	public CustomerFileDAO() throws IOException {
+		File file = new File("customer.db");
+		this.writer = new PrintWriter(new FileWriter(file, true));
+		this.scanner = new Scanner(file);
+
+	}
+	
+	public void create(Customer customer) {
+		writer.println("--------------");
+		writer.println(customer.getName());
+		writer.println(customer.getAddress());
+		writer.println("--------------");
+		writer.flush();
+	}
+	public Customer read() {
+		
+			Customer customer = new Customer();
+			scanner.nextLine();
+
+			String name = scanner.nextLine();
+			String address = scanner.nextLine();
+			customer.setName(name);
+			customer.setAddress(address);
+			scanner.nextLine();
+			return customer;
 	
 
+	}
+	
+	public List<Customer> search(String criterion) throws FileNotFoundException{
+		List<Customer> customers = new ArrayList<Customer>();
+		this.scanner = new Scanner(new File("customer.db"));
+		while (this.scanner.hasNext()) {
+			Customer currentCustomer = read();
+			//display only customers whose adresses are like the filter
+			if (currentCustomer.getAddress().equals(criterion)) {
+				customers.add(currentCustomer);
+			}
+		}
+		return customers;
+	}
+	
+	
 }
--- DAOExercises/test/fr/epita/tests/TestDataAccessFromFile.java
+++ DAOExercises/test/fr/epita/tests/TestDataAccessFromFile.java
@@ -1,33 +1,62 @@
 package fr.epita.tests;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 import fr.epita.datamodel.Customer;
+import fr.epita.services.dao.CustomerFileDAO;
 
 public class TestDataAccessFromFile {
 
 	public static void main(String[] args) throws IOException {
-		//testWriteCustomer();
-		
+		// testWriteCustomer();
+
+		// testReadCustomer();
+
+		// read from the console then write the user inputs in the file
+		boolean exit = false;
+		Scanner scanner = new Scanner(System.in);
+		CustomerFileDAO dao = new CustomerFileDAO();
+		do {
+
+			System.out.println("Please input Customer name");
+			String name = scanner.nextLine();
+			System.out.println("Please input Customer address");
+			String address = scanner.nextLine();
+			Customer customer = new Customer(name, address);
+			dao.create(customer);
+			System.out.println("Enter a City name to filter customers  :");
+			String criterion = scanner.nextLine();
+			System.out.println("Customer list is :");
+			System.out.println(dao.search(criterion));
+			System.out.println("Do you want to exit?");
+			String answer = scanner.nextLine();
+			exit = answer.equals("y");
+
+		} while (!exit);
+
+		scanner.close();
+
+	}
+
+	private static void testReadCustomer() throws FileNotFoundException {
 		File file = new File("customer.db");
-		
+
 		Scanner scanner = new Scanner(file);
 		Customer customer = readCustomer(scanner);
 		System.out.println(customer);
-		
-		
 	}
 
 	private static Customer readCustomer(Scanner scanner) {
 		Customer customer = new Customer();
 		scanner.nextLine();
-		
+
+		String name = scanner.nextLine();
 		String address = scanner.nextLine();
-		String name =scanner.nextLine();
 		customer.setName(name);
 		customer.setAddress(address);
 		scanner.nextLine();
@@ -37,11 +66,11 @@ public class TestDataAccessFromFile {
 	private static void testWriteCustomer() throws IOException {
 		File file = new File("customer.db");
 		PrintWriter writer = new PrintWriter(new FileWriter(file, true));
-				
-		//writing a customer (second option)
+
+		// writing a customer (second option)
 		Customer customer = new Customer("Thomas", "Paris");
 		writeCustomer(writer, customer);
-		
+
 		writer.close();
 	}
 
@@ -50,6 +79,7 @@ public class TestDataAccessFromFile {
 		writer.println(customer.getName());
 		writer.println(customer.getAddress());
 		writer.println("--------------");
+		writer.flush();
 	}
 
 }
