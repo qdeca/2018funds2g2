517d2ec1b96a3dfaebf99367e6b40aed8d3e671f
Decayeux <qdecayeux@natsystem.fr>
2019-01-15 20:24:24 +0100

    xml

--- QuizzManager/src/fr/epita/services/dao/QuestionXMLDAO.java
+++ QuizzManager/src/fr/epita/services/dao/QuestionXMLDAO.java
@@ -1,6 +1,14 @@
 package fr.epita.services.dao;
 
+import fr.epita.datamodel.Question;
+
 public class QuestionXMLDAO {
 
+	public void create(Question question) {
+		
+	}
 	
+	public void update(Question question) {
+		
+	}
 }
---
+++ QuizzManager/tests/fr/epita/tests/QuestionXMLDAOTest.java
@@ -0,0 +1,5 @@
+package fr.epita.tests;
+
+public class QuestionXMLDAOTest {
+
+}
