e73859ff9ad90d49b389e8ffb390f33aef19f44e
Decayeux <qdecayeux@natsystem.fr>
2019-01-21 18:55:38 +0100

    xml read  example

--- QuizzManager/tests/fr/epita/tests/QuestionXMLDAOTest.java
+++ QuizzManager/tests/fr/epita/tests/QuestionXMLDAOTest.java
@@ -1,5 +1,34 @@
 package fr.epita.tests;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class QuestionXMLDAOTest {
 
+	
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+		DocumentBuilderFactory fact = DocumentBuilderFactory.newInstance(); 
+		DocumentBuilder builder = fact.newDocumentBuilder(); // builder used to parse the xml file
+		
+		Document doc = builder.parse(new File("questions.xml")); //extraction of the data in xml 
+																// and put in the doc object
+		NodeList listQuestions = doc.getElementsByTagName("question");  // get all the elements going by the "question" tag
+		for (int i=0; i<listQuestions.getLength(); i++) {
+			Element question = (Element) listQuestions.item(i);
+			String order = question.getAttribute("order");   //get the "order" attribute of the element
+			System.out.println("Order : " + order);
+			NodeList listLabels = question.getElementsByTagName("label"); //get all the elements of the "label" tag (here, only one)
+			String label = listLabels.item(0).getTextContent(); // get the text content of the label element
+			System.out.println("Question : " + label);
+		}
+	}
 }
